3217e3da6d89a11a84d184f98cb18f8ee9a9ed37
Shareen Chang <shareenchang@D-10-18-188-237.dhcp4.washington.edu>
2019-05-23 20:50:50 -0700

    Graphed stacked barchart

--- scripts/stacked_bar_chart.R
+++ scripts/stacked_bar_chart.R
@@ -1,1 +1,20 @@
 # Stacked bar chart
+filtered_flights <- read.csv("data/american_and_delta_airlines.csv", stringsAsFactors = F)
+
+total_flights <- nrow(filtered_flights)
+
+compare_airlines <- filtered_flights %>%
+  group_by(MONTH, AIRLINE) %>%
+  summarize(count = n())
+
+# american_flights <- filtered_flights %>%
+#   filter(AIRLINE == "AA") %>%
+#   group_by(MONTH) %>%
+#   summarize(count = n())
+# 
+# delta_flights <- filtered_flights %>%
+#   filter(AIRLINE == "DL") %>%
+#   group_by(MONTH) %>%
+#   summarize(count = n())
+# 
+# compare_flights <- left_join(american_flights, delta_flights, by = "MONTH")

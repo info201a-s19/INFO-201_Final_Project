fc822af7a43e11ed788ab23cb791fdc382fec30a
Shareen Chang <shareenchang@D-10-18-188-237.dhcp4.washington.edu>
2019-05-23 22:49:06 -0700

    Deleted unnessary comments

--- scripts/stacked_bar_chart.R
+++ scripts/stacked_bar_chart.R
@@ -6,15 +6,3 @@ total_flights <- nrow(filtered_flights)
 compare_airlines <- filtered_flights %>%
   group_by(MONTH, AIRLINE) %>%
   summarize(count = n())
-
-# american_flights <- filtered_flights %>%
-#   filter(AIRLINE == "AA") %>%
-#   group_by(MONTH) %>%
-#   summarize(count = n())
-# 
-# delta_flights <- filtered_flights %>%
-#   filter(AIRLINE == "DL") %>%
-#   group_by(MONTH) %>%
-#   summarize(count = n())
-# 
-# compare_flights <- left_join(american_flights, delta_flights, by = "MONTH")

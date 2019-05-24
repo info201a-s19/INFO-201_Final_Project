b48b19449429f68a8042433da5733f41ff039564
Jerome Orille <jcorille@uw.edu>
2019-05-24 14:30:31 -0700

    organized information

--- scripts/us_map.R
+++ scripts/us_map.R
@@ -1,11 +1,13 @@
-# Density map of number of airports in the US
-us_airports <- read.csv("data/airports.csv", stringsAsFactors = F)
-
 # Loaded libraries
 library("dplyr")
 library("ggplot2")
 library("plotly")
 library("tidyr")
+library("leaflet")
+library("lintr")
+
+# Density map of number of airports in the US
+us_airports <- read.csv("data/airports.csv", stringsAsFactors = F)
 
 only_us_airports <- us_airports %>%
   filter(iso_country == "US", longitude_deg < -50, latitude_deg > 15)
@@ -15,4 +17,18 @@ types <- only_us_airports %>%
   summarize(count = n())
 
 airports <- only_us_airports %>%
-  filter(type == "small_airport" | type == "medium_airport" | type == "large_airport")
+  filter(type == "small_airport" |
+           type == "medium_airport" |
+           type == "large_airport")
+
+us_map <- leaflet(data = airports) %>%
+  addTiles() %>%
+  addCircleMarkers(
+    lat = ~latitude_deg,
+    lng = ~longitude_deg,
+    popup = ~paste("Name:", name, "<br>",
+                   "Type:", type, "<br>",
+                   "Elevation:", elevation_ft),
+    radius = 0.01,
+    weight = 0
+)

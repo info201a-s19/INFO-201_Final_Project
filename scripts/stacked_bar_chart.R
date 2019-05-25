0fb02497de1b4f34040af69421d78e78b15c3a92
Jerome Orille <jcorille@uw.edu>
2019-05-24 18:35:51 -0700

    added insights

--- scripts/stacked_bar_chart.R
+++ scripts/stacked_bar_chart.R
@@ -32,7 +32,42 @@ num_flights_chart <- ggplot(data = compare_airlines,
                               "Nov",
                               "Dec")) +
   scale_fill_discrete(name = "Airlines", labels = c("American Airlines (AA)",
-                                                    "Delta Airlines(DL)")) +
+                                                    "Delta Airlines (DL)")) +
   scale_fill_manual(values = alpha(c("lightblue", "pink"), 1)) +
   geom_bar(stat = "identity") +
   theme(plot.title = element_text(hjust = 0.5)) # Center title
+
+# Calculating insights of data
+num_month_with_most_aa_flights <- compare_airlines %>%
+  filter(AIRLINE == "AA") %>%
+  arrange(-count) %>%
+  head(1) %>%
+  pull(MONTH)
+
+month_with_most_aa_flights <- month.name[num_month_with_most_aa_flights]
+
+num_month_with_most_dl_flights <- compare_airlines %>%
+  filter(AIRLINE == "DL") %>%
+  arrange(-count) %>%
+  head(1) %>%
+  pull(MONTH)
+
+month_with_most_dl_flights <- month.name[num_month_with_most_dl_flights]
+
+num_month_with_most_flights <- compare_airlines %>%
+  group_by(MONTH) %>%
+  summarize(count = sum(count)) %>%
+  arrange(-count) %>%
+  head(1) %>%
+  pull(MONTH)
+
+month_with_most_flights <- month.name[num_month_with_most_flights]
+
+num_month_with_least_flights <- compare_airlines %>%
+  group_by(MONTH) %>%
+  summarize(count = sum(count)) %>%
+  arrange(count) %>%
+  head(1) %>%
+  pull(MONTH)
+
+month_with_least_flights <- month.name[num_month_with_least_flights]

29e2dd60e1e2659fd151903770000786dd8577c4
Shareen Chang <shareenchang@Shareens-MacBook-Pro-2.local>
2019-05-24 17:31:19 -0700

    Made small changes

--- scripts/us_map.R
+++ scripts/us_map.R
@@ -1,5 +1,5 @@
 # Density map of number of airports in the US
-us_airports <- read.csv("data/airports.csv", stringsAsFactors = F)
+us_airports <- read.csv("data/american_and_delta_airlines.csv", stringsAsFactors = F)
 
 # Loaded libraries
 library("dplyr")
@@ -7,12 +7,12 @@ library("ggplot2")
 library("plotly")
 library("tidyr")
 
-only_us_airports <- us_airports %>%
-  filter(iso_country == "US", longitude_deg < -50, latitude_deg > 15)
-
-types <- only_us_airports %>%
-  group_by(type) %>%
-  summarize(count = n())
-
-airports <- only_us_airports %>%
-  filter(type == "small_airport" | type == "medium_airport" | type == "large_airport")
+# only_us_airports <- us_airports %>%
+#   filter(iso_country == "US", longitude_deg < -50, latitude_deg > 15)
+# 
+# types <- only_us_airports %>%
+#   group_by(type) %>%
+#   summarize(count = n())
+# 
+# airports <- only_us_airports %>%
+#   filter(type == "small_airport" | type == "medium_airport" | type == "large_airport")

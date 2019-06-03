13de41a4e6be357cb0e7042e967fd5de903d5108
Jerome Orille <jcorille@uw.edu>
2019-06-03 15:58:47 -0700

    added variable names

--- app.R
+++ app.R
@@ -3,7 +3,6 @@ library("dplyr")
 library("ggplot2")
 library("plotly")
 
-source("app_ui.R")
 source("app_server.R")
 
-shinyApp(ui = ui, server = server)
+shinyApp(ui = proj_ui, server = proj_server)
